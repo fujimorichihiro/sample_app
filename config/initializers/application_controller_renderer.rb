bb7c95e73f2254eb52d90b5ed3dd2e9cbd3bc2ce
fujimorichihiro <fujimori.drum@gmail.com>
2020-04-04 22:51:39 +0900

    [Finish]3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '29b948f4a0901b4b2e41661d80e3a1577075fd37e67b21f34333b7d7a814ff6560f82a1126674f13a1c3693d348c437956a3caa9290f8ba7bb7e68f198f6f2ef'
